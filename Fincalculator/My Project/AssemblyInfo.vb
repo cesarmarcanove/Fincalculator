b45419fa12ea992594c2a261af289ee5c2d49875
cesar marcano <cesarmarcanove@users.noreply.github.com>
2020-11-11 16:45:51 -0400

    Add files via upload

---
+++ Fincalculator/My Project/AssemblyInfo.vb
@@ -0,0 +1,35 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' La información general sobre un ensamblado se controla mediante el siguiente 
+' conjunto de atributos. Cambie estos atributos para modificar la información
+' asociada con un ensamblado.
+
+' Revisar los valores de los atributos del ensamblado
+
+<Assembly: AssemblyTitle("Fincalculator")> 
+<Assembly: AssemblyDescription("FinCalculator, suite combo de programas financieros básicos para el usuario común y sin requerir usos avanzados.")> 
+<Assembly: AssemblyCompany("Creado por Ing. Cesar Marcano")> 
+<Assembly: AssemblyProduct("Fincalculator")> 
+<Assembly: AssemblyCopyright("Copyright ©  2020")> 
+<Assembly: AssemblyTrademark("")> 
+
+<Assembly: ComVisible(False)>
+
+'El siguiente GUID sirve como identificador de typelib si este proyecto se expone a COM
+<Assembly: Guid("0fe77823-ad20-47e1-90b3-ecdeefbaf378")> 
+
+' La información de versión de un ensamblado consta de los cuatro valores siguientes:
+'
+'      Versión principal
+'      Versión secundaria 
+'      Número de versión de compilación
+'      Revisión
+'
+' Puede especificar todos los valores o establecer como predeterminados los números de versión de compilación y de revisión 
+' mediante el asterisco ('*'), como se muestra a continuación:
+' <Assembly: AssemblyVersion("1.0.*")> 
+
+<Assembly: AssemblyVersion("1.3.0.0")> 
+<Assembly: AssemblyFileVersion("1.3.0.0")> 
